a5301d9a47436e0dda3527e0b6bb576ee56474d1
Velina Petrova <velinarp@gmail.com>
2020-11-25 20:54:19 +0800

    add algorithm to randomly generate the value of the tiles
    Signed-off-by: Velina Petrova <velinarp@gmail.com>

--- lib/tiles_web/live/page_live.ex
+++ lib/tiles_web/live/page_live.ex
@@ -1,10 +1,29 @@
 defmodule TilesWeb.PageLive do
   use TilesWeb, :live_view
 
-  @number_of_tiles 16
+  @number_of_pairs 8
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, number_of_tiles: @number_of_tiles)}
+    socket =
+      socket
+      |> assign(tiles: generate_tiles())
+
+    {:ok, socket}
+  end
+
+  defp generate_tiles do
+    generate_values()
+    |> Enum.map(&(%{state: "closed", value: &1}))
+  end
+
+  defp generate_values do
+    values()
+    |> Enum.concat(values())
+    |> Enum.shuffle()
+  end
+
+  defp values do
+    Enum.to_list(1..@number_of_pairs)
   end
 end
