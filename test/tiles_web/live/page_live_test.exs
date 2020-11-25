9c66988b0e007966ac0bc76ab7c2e009eb50fec7
Velina Petrova <velinarp@gmail.com>
2020-11-25 19:12:50 +0800

    clean up code
    Signed-off-by: Velina Petrova <velinarp@gmail.com>

--- test/tiles_web/live/page_live_test.exs
+++ test/tiles_web/live/page_live_test.exs
@@ -1,11 +1,5 @@
-defmodule TilesWeb.PageLiveTest do
+defmodule TilesWeb.PagseLiveTest do
   use TilesWeb.ConnCase
 
-  import Phoenix.LiveViewTest
-
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
-  end
+  import Phoenix.LiveViewTests
 end
